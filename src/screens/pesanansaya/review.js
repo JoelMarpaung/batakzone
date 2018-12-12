5407a070c7931d3e1bfb1f817788c6ba8fdd1aaf
hanspurba <if415033@students.del.ac.id>
2018-12-12 19:46:53 +0700

    menyelesaikan tampilan review setelah pembelian

--- src/screens/pesanansaya/review.js
+++ src/screens/pesanansaya/review.js
@@ -19,7 +19,8 @@ import {
   Input,
   Left,
   Body,
-  Right
+  Right,
+  Row
 } from "native-base";
 import styles from "./styles";
 import { black } from "ansi-colors";
@@ -34,12 +35,19 @@ class Review extends Component {
       <Container>
         <Content padder>
         <Form>
+          <Row style={{marginTop:10}}>
+            <Icon name="star" style={{fontSize:20}}/>
+            <Icon name="star" style={{fontSize:20}}/>
+            <Icon name="star" style={{fontSize:20}}/>
+            <Icon name="star" style={{fontSize:20}}/>
+            <Icon name="star" style={{fontSize:20}}/>
+          </Row>
           <Label style={{marginTop:30, color:"black", fontSize: 18, marginBottom:15}}>Ulasan</Label>
               <Card style={styles.review}>
                 <Input keyboardType="string" />
               </Card>
-            <Button rounded style={styles.buttonBottom}>
-                <Text style={{ color: "white", fontSize: 12}}>Simpan</Text>
+            <Button rounded style={styles.buttonBawah}>
+                <Text style={{ color: "white", fontSize: 15}}>Simpan</Text>
               </Button>
         {/* <Card style={styles.mt}> */}
           {/* <H3 style={{color:"#8E171B",marginLeft:10,marginTop:10, fontWeight:'bold'}}>Toko Bintang Terang</H3> */}
